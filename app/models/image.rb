49afb3d98c3b9fe561316c6054562c1daca95f0c
Alex Hunley <ahunley@nutshell.com>
2018-02-04 10:01:50 -0500

    :paperclip: Add paperclip helpers to Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,9 @@
 class Image < ApplicationRecord
   belongs_to :imageable, polymorphic: true
   enum role: [:default, :hero]
+
+  has_attached_file :attachment,
+                    styles: { thumb: '100x100>' },
+                    default_url: '/images/:styles/missing.png'
+  validates_attachment_content_type :attachment, content_type: /\Aimage\/.*\z/
 end
